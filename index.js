3898adf3f27e2ad9a010383e2de6e43835631d3a
Justin Chiou <chiou.kai@gmail.com>
2024-10-08 15:00:22 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import fs from "node:fs";
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { get, ref, getDatabase } from "firebase/database";
+
+const firebaseConfig = { /** insert config */ };
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+async function getCompetitors() {
+  const competitorsRef = ref(db, "competitors");
+  const snapshot = await get(competitorsRef);
+  if (!snapshot.exists) {
+    throw "no data available"
+  }
+  return snapshot.val();
+}
+
+function getEmailList(competitors) {
+  const emails = Object.values(competitors).map((c) => c.email);
+  const unique = new Set();
+  emails.forEach((email) => unique.add(email));
+  return Array.from(unique);
+}
+
+async function main() {
+  const competitors = await getCompetitors();
+  const list = getEmailList(competitors);
+  fs.writeFileSync("emails.txt", list.join("\n"), "utf-8");
+  process.exit(0);
+}
+
+main();
